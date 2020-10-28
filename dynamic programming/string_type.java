c61a350b5a8cde70d142bed4c38344d75d1421a0
Alankrit <alankritbhaskar1@gmail.com>
2020-10-28 22:30:55 +0530

    update

--- dynamic programming/string_type.java
+++ dynamic programming/string_type.java
@@ -2,35 +2,82 @@ import java.util.*;
 
 public class string_type{
 
-// Leetcode 647 Count all palindromic substrings
-
-    public int countSubstrings(String s) {
-    int n=s.length();
-    boolean dp[][]=new boolean[n][n];
-    palindromicSubstring(s,dp);
-        int count=0;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            if(dp[i][j]==true)
-                count++;
-        }
-    }
-        return count;
-    }
-    public static void palindromicSubstring(String s,boolean dp[][]){
-        int n=s.length();
+    public static void print(int[] arr){
+        for(int ele: arr)
+          System.out.print(ele + " "); 
         
-        for(int gap=0;gap<n;gap++){
-            for(int i=0,j=gap;i<n&&j<n;i++,j++){
-                if(gap==0)
-                    dp[i][j]=true;
-                else if(gap==1)
-                    dp[i][j]=s.charAt(i)==s.charAt(j);
-                else
-                    dp[i][j]=s.charAt(i)==s.charAt(j) && dp[i+1][j-1];
+        System.out.println();
+    }
+
+    public static void print2D(int[][] arr){
+        for(int[] a: arr)
+          print(a);
+
+        System.out.println();
+    }
+
+    public static void palindromicSubstring(String str,boolean[][] dp){
+        int n = str.length();
+        for(int gap = 0;gap < n;gap++){
+            for(int i=0,j=gap; j < n;i++,j++){
+                if(gap == 0) dp[i][j] = true;
+                else if(gap == 1) dp[i][j] = str.charAt(i) == str.charAt(j);
+                else dp[i][j] = str.charAt(i) == str.charAt(j)  && dp[i+1][j-1];
             }
         }
+    } 
+
+
+    //Leetcode 647
+    public int countSubstrings(String s) {
+        int n = str.length();
+        boolean[][] dp = new boolean[n][n];
+        int count = 0; 
+        for(int gap = 0;gap < n;gap++){
+            for(int i=0,j=gap; j < n;i++,j++){
+                if(gap == 0) dp[i][j] = true;
+                else if(gap == 1) dp[i][j] = str.charAt(i) == str.charAt(j);
+                else dp[i][j] = str.charAt(i) == str.charAt(j)  && dp[i+1][j-1];
+            
+                if(dp[i][j]) count++; 
+            }
+        }
+        return count;
     }
 
+// Leetcode 005:- Longest palindromic substring
+
+    public String longestPalindrome(String str) {
+        int n = str.length();
+        int[][] dp = new int[n][n];
+        
+        int si = 0,ei = 0,length = 0; // starting index, ending index of longest palindromic susbtring.
+        
+        for(int gap = 0;gap < n;gap++){
+            for(int i=0,j=gap; j < n;i++,j++){
+
+                if(gap == 0)
+                dp[i][j] = 1;
+                else if(gap == 1 && str.charAt(i) == str.charAt(j)) 
+                dp[i][j] = 2;
+                else if(str.charAt(i) == str.charAt(j) && dp[i+1][j-1] > 0)
+                dp[i][j] = dp[i+1][j-1] + 2;
+            
+                if(dp[i][j] > length){
+                    length = dp[i][j];
+                    si = i;
+                    ei = j;
+                } 
+            }
+        }
+
+        return str.substring(si,ei+1);
+    }
+
+    public static void main(String args[]){
+
+        
+    }
 
 }
+
