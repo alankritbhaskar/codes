927c06c8af704976a41b7db96aa6591b24bbb5de
Alankrit <alankritbhaskar1@gmail.com>
2020-10-30 12:21:12 +0530

    update

--- matrix/ques.java
+++ matrix/ques.java
@@ -137,7 +137,86 @@ public class ques{
         return matrix;
 }
 
+//Leetcode-48: Rotate Matrix
 
+    // Using extra space----
+    
+    public void rotateExtra(int[][] matrix) {
+        int m=matrix.length;int n=matrix[0].length;
+        
+        int mat[][]=new int[m][n];
+        
+        for(int j=n-1;j>=0;j--){
+            for(int i=0;i<m;i++){
+                mat[i][j]=matrix[n-j-1][i];
+            }
+        }
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                matrix[i][j]=mat[i][j];
+            }
+        }
+    }
+    
+// Without using extra space----
+    
+/* clockwise rotate
+* first reverse up to down, then swap the symmetry 
+* 1 2 3     7 8 9     7 4 1
+* 4 5 6  => 4 5 6  => 8 5 2
+* 7 8 9     1 2 3     9 6 3
+*/
+    
+/*
+ * anticlockwise rotate
+ * first reverse left to right, then swap the symmetry
+ * 1 2 3     3 2 1     3 6 9
+ * 4 5 6  => 6 5 4  => 2 5 8
+ * 7 8 9     9 8 7     1 4 7
+*/
+    
+//CPP:-
+
+// void rotate(vector<vector<int> > &matrix) {
+//     reverse(matrix.begin(), matrix.end());
+//     for (int i = 0; i < matrix.size(); ++i) {
+//         for (int j = i + 1; j < matrix[i].size(); ++j)
+//             swap(matrix[i][j], matrix[j][i]);
+//     }
+// }
+
+
+// void anti_rotate(vector<vector<int> > &matrix) {
+//     for (auto vi : matrix) reverse(vi.begin(), vi.end());
+//     for (int i = 0; i < matrix.size(); ++i) {
+//         for (int j = i + 1; j < matrix[i].size(); ++j)
+//             swap(matrix[i][j], matrix[j][i]);
+//     }
+// }
+    
+    void rotate(int matrix[][]){
+    
+    int sr=0,er=matrix.length-1;
+        
+    while(sr<er){
+        int[] temp = matrix[sr];
+        matrix[sr] = matrix[er];
+        matrix[er] = temp;
+        sr++; 
+        er--;
+    }
+
+    for(int i = 0; i< matrix.length ; i++){
+        for(int j = 0;j < i; j++){
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+        }
+    }        
+    }
+
+    
 
 
     public static void main(String args[]){
