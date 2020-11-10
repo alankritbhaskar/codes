b30e5d7ba84f59ac27f27c8d31931f861c036814
Alankrit <alankritbhaskar1@gmail.com>
2020-11-10 23:33:09 +0530

    questions added

--- stack & queues/questions.cpp
+++ stack & queues/questions.cpp
@@ -83,6 +83,116 @@ st.push(i);
 return ans;
 }
 
+// Leetcode 20:- Valid Parenthesis
+
+/*
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', 
+determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets. Open brackets must be closed in the correct order.
+ 
+Example 1:
+Input: s = "()"
+Output: true
+
+Example 2:
+Input: s = "()[]{}"
+Output: true
+*/
+
+    bool isValid(string s) {
+        
+        stack<char> st;
+        
+        for(char c: s){
+            if(c=='(' || c=='{' || c=='[')
+            st.push(c);
+            else if(c==')'){
+            if(st.size()!=0 && st.top()=='(')
+                st.pop();
+                else
+                    return false;
+            }
+            else if(c=='}'){
+                if(st.size()!=0 && st.top()=='{')
+                    st.pop();
+                else
+                    return false;
+            }
+            else if(c==']'){
+                if(st.size()!=0 && st.top()=='[')
+                    st.pop();
+                else
+                    return false;
+            }
+            }
+            return st.size()==0;
+    }
+
+// Leetcode 503:- Next Greater Element II
+
+/*
+Given a circular array (the next element of the last element is the first element of the array),
+print the Next Greater Number for every element. \
+The Next Greater Number of a number x is the first greater number to its traversing-order next in the array, 
+which means you could search circularly to find its next greater number. 
+If it doesn't exist, output -1 for this number.
+
+Example 1:
+Input: [1,2,1]
+Output: [2,-1,2]
+Explanation: The first 1's next greater number is 2; 
+The number 2 can't find next greater number; 
+The second 1's next greater number needs to search circularly, which is also 2.
+*/
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> ans(n,-1);
+        
+        stack<int> st;
+        for(int i= 0; i< 2*n; i++){
+            while(st.size()!=0 && nums[st.top()]<nums[i%n]){
+                ans[st.top()]=nums[i%n];
+                st.pop();
+            }
+            
+            if(i<n)
+                st.push(i);
+        }
+        return ans;
+    }
+
+// Leetcode 946:- Validate Stack Sequences
+/*
+Given two sequences pushed and popped with distinct values, 
+return true if and only if this could have been the result of a sequence of push and pop operations 
+on an initially empty stack.
+
+Example 1:
+
+Input: pushed = [1,2,3,4,5], popped = [4,5,3,2,1]
+Output: true
+Explanation: We might do the following sequence:
+push(1), push(2), push(3), push(4), pop() -> 4,
+push(5), pop() -> 5, pop() -> 3, pop() -> 2, pop() -> 1
+*/
+
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+       
+        int n=pushed.size();
+        int idx=0;
+        stack<int> st;
+        for(int i =0;i< n;i++ ){
+            st.push(pushed[i]);
+            while(st.size()!=0 && popped[idx]==st.top()){
+                st.pop();idx++;
+            }
+        }
+        
+        return st.size()==0;
+    }
 
 int main(int argc, const char** argv) {
     
