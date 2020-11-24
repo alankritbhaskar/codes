8923a86c31871c5d1f79d1084421e39e8e8c5df1
Alankrit <alankritbhaskar1@gmail.com>
2020-11-24 18:53:56 +0530

    update

--- linkedlist/questions.java
+++ linkedlist/questions.java
@@ -72,6 +72,88 @@ public class questions{
         return head;
     }
 
+// Leetcode 234:- Palindrome Linked List
+
+    public ListNode midNode(ListNode node){
+        if(node==null || node.next==null)
+            return node;
+        
+        ListNode slow=node,fast=node;
+        
+        while(fast.next!=null && fast.next.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        
+        return slow;
+    }
+    
+    public ListNode reverse(ListNode node){
+    if(node==null || node.next==null)
+        return node;
+        
+    ListNode prev=null,curr=node;
+    while(curr!=null){
+        ListNode fwd=curr.next;
+        curr.next=prev;
+        prev=curr;
+        curr=fwd;
+    }
+        return prev;
+    }
+    
+    public boolean isPalindrome(ListNode node) {
+    if(node==null || node.next==null) // 0/1 nodes in list
+        return true;
+    
+    ListNode mid=midNode(node);
+    ListNode newHead=mid.next;
+    mid.next=null;
+    
+    newHead=reverse(newHead);
+    ListNode c1=node,c2=newHead;
+    
+    boolean res=true;
+    while(c1!=null && c2!=null){
+        if(c1.val!=c2.val)
+        {
+            res=false;
+            break;
+        }
+        c1=c1.next;
+        c2=c2.next;
+    }
+        
+    newHead=reverse(newHead);
+    mid.next=newHead;
+    
+    return res;
+    }
+
+// Leetcode 143. Reorder List
+
+ public void reorderList(ListNode head) {
+    if(head==null || head.next==null)
+            return;
+        
+    ListNode mid = midNode(head);
+    ListNode nhead = mid.next;
+    mid.next = null;
+    nhead = reverse(nhead);
+    
+    ListNode c1 = head,c2 = nhead;
+    while(c1 != null && c2 != null){
+        ListNode f1 = c1.next; // backup
+        ListNode f2 = c2.next;
+        
+        c1.next = c2;
+        c2.next = f1;
+        
+        c1 = f1;
+        c2 = f2;
+    }
+    }
+
     
 
 
