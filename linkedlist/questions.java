cd70cb66c48fc179732ae3c791f1c7a0035afc6d
Alankrit <alankritbhaskar1@gmail.com>
2021-04-15 18:35:00 +0530

    update

--- linkedlist/questions.java
+++ linkedlist/questions.java
@@ -56,21 +56,48 @@ public class questions{
         return prev;
     }
 
-    public ListNode reverseList(ListNode node) {
-        if(node==null || node.next==null)
-            return node;
+// Using recursion
+
+    public ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null)
+            return head;
         
-        ListNode node1 = reverseList(node.next);
-        ListNode head=node1;
-        
-        while(node1.next!=null)
-            node1=node1.next;
-        
-        node1.next = node; 
-        node.next = null; 
-    
-        return head;
+        ListNode temp = head.next;
+        head.next = null;
+        ListNode reversedList = reverseList(temp);
+        temp.next = head;
+        return reversedList;
     }
+
+// Reverse Data in a LinkedList
+    
+    void dataReverse(ListNode head){
+    if (head == null || head.next == null)
+        return;
+
+    ListNode mid = midNode(head);
+    ListNode nhead = mid.next;
+    mid.next = null;
+
+    nhead = reverseList(nhead);
+
+    ListNode curr1 = head;
+    ListNode curr2 = nhead;
+
+    while (curr1 != null && curr2 != null)
+    {
+
+        int temp = curr1.val;
+        curr1.val = curr2.val;
+        curr2.val = temp;
+
+        curr1 = curr1.next;
+        curr2 = curr2.next;
+    }
+
+    nhead = reverseList(nhead);
+    mid.next = nhead;
+   }
 
 // Leetcode 234:- Palindrome Linked List
 
@@ -154,15 +181,88 @@ public class questions{
     }
     }
 
+// Seggregate even and odd nodes..... https://practice.geeksforgeeks.org/problems/segregate-even-and-odd-nodes-in-a-linked-list5035/1
+
+    Node divide(int N, Node head){
+        Node dummyOdd = new Node(-1);
+        Node dummyEven = new Node(-1);
+        Node odd = dummyOdd;
+        Node even = dummyEven;
+
+        Node curr = head;
+        while (curr != null) {
+            if (curr.data % 2 != 0) {
+                odd.next = curr;
+                odd = odd.next;
+            } else {
+                even.next = curr;
+                even = even.next;
+            }
+
+            curr = curr.next;
+        }
+
+        even.next = dummyOdd.next;
+        odd.next = null;
+
+        return dummyEven.next;
+    }
+
+// Unfold a folded linked list
+/
+A linked list L0 . L1 . L2 . ….. . LN can be folded as L0 . LN . L1 . LN – 1 . L2 . ….. 
+Given a folded linked list, the task is to unfold and print the original linked list
+
+Examples:  
+
+Input: 1 . 6 . 2 . 5 . 3 . 4 
+Output: 1 2 3 4 5 6
+
+Input: 1 . 5 . 2 . 4 . 3 
+Output: 1 2 3 4 5 
+/
+
+// Using Recursion
+
+
+// Using Iteration
+
+    void againReorderList(ListNode head){
+        if (head == null || head.next == null)
+           return;
+
+    ListNode h1 = head;
+    ListNode h2 = head.next;
+
+    ListNode c1 = h1;
+    ListNode c2 = h2;
+
+    while (c2 != null && c2.next != null)
+    {
+        ListNode f = c2.next; // Backup
+
+        c1.next = f; // links
+        c2.next = f.next;
+
+        c1 = c1.next;
+        c2 = c2.next;
+    }
+
+    // c1.next = null;
+    h2 = reverseList(h2);
+    c1.next = h2;
+    }
+
+
 // Leetcode 19. Remove nth from end of list
 
-public ListNode removeNthFromEnd(ListNode head, int n) {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
         if(head==null || head.next==null)
             return null;
         
         ListNode c1=head,c2=head;
         
-        while(n-->0)
+        while(n-.0)
             c2=c2.next;
         
         if(c2==null)
@@ -215,7 +315,7 @@ public ListNode removeNthFromEnd(ListNode head, int n) {
         
     }
 
-// Leetcode 148. Sort List -----> nlog(n)
+// Leetcode 148. Sort List ----. nlog(n)
 
     public ListNode middle(ListNode head){
         if(head==null || head.next==null)
@@ -305,7 +405,7 @@ public ListNode removeNthFromEnd(ListNode head, int n) {
         
     }
     
-    // O(kN) time complexity---> approach 1
+// O(kN) time complexity--. approach 1
     
     public ListNode mergeKLists(ListNode[] lists) {
     
@@ -329,7 +429,21 @@ public ListNode removeNthFromEnd(ListNode head, int n) {
         return list.get(0);
     }
 
-    // Divide and Conquer approach--- O(n log k)..... approach 2
+// O(k2*N)
+// where, n = avg. nodes in each linked list || k = length of lists array
+
+    public ListNode mergek(ListNode lists[]){
+        if(lists.length == 0)
+            return null;
+        
+        ListNode ref = null;
+        for(int i = 0;i < lists.length;i++)
+            ref = merge2(ref,lists[i]);
+
+        return ref;
+    }
+
+// Divide and Conquer approach--- O(n log k)..... approach 2
 
  public ListNode mergeKLists(ListNode[] lists) {
         if(lists.length==0)
@@ -351,6 +465,37 @@ public ListNode removeNthFromEnd(ListNode head, int n) {
         
         return l3;
     }
+
+// Using Priority Queue ---> O(k*N*logk)
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode> pq = new PriorityQueue<>((a,b)->{
+            return a.val - b.val; // this - other, default behaviour
+        });
+        
+        ListNode dummy = new ListNode(-1);
+        ListNode prev = dummy;
+        
+        // k logk
+        for(ListNode l: lists){
+            if(l != null)
+                pq.add(l);
+        }
+        
+        //k*N logk
+        while(pq.size() != 0){
+            ListNode node = pq.remove();
+            prev.next = node;
+            prev = node;
+            node = node.next;
+            
+            if(node != null)
+                pq.add(node);
+        }
+        
+        return dummy.next;
+    }
+
 
 
 }
