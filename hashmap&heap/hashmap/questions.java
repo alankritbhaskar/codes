6999f2bb8d957b408c797626eb4b36399ea270dd
Alankrit <alankritbhaskar1@gmail.com>
2021-05-06 00:33:06 +0530

    updates

--- hashmap&heap/hashmap/questions.java
+++ hashmap&heap/hashmap/questions.java
@@ -100,6 +100,84 @@ public class questions{
         return len;
     }
 
-//
+// 347. Top K Frequent Elements
+
+/*
+Given an integer array nums and an integer k, return the k most frequent elements. 
+You may return the answer in any order.
+
+Example 1:
+
+Input: nums = [1,1,1,2,2,3], k = 2
+Output: [1,2]
+*/
+
+    public int[] topKFrequent(int[] nums, int k) {
+     int n = nums.length;
+     if(n == 0)
+        return new int[]{0};
+
+     HashMap<Integer,Integer> map = new HashMap<>();
+     for(int ele: nums)
+        map.put(ele,map.getOrDefault(ele,0)+1);
+
+     int idx = 0;
+     int arr[] = new int[k];
+
+     PriorityQueue<int []> pq = new PriorityQueue<>((a,b)->{
+         return a[1] - b[1]; // this-other -> default, minHeap
+     });
+
+     for(Integer key: map.keySet()){
+         pq.add(new int[]{key,map.get(key)});
+         if(pq.size() > k)
+         pq.remove();
+     }
+
+     while(pq.size() > 0){
+         int pair[] = pq.peek();
+         int ele = pair[0];
+         arr[idx++] = ele;
+         pq.remove();
+     }
+
+     return arr;   
+    }
+
+// 973. K Closest Points to Origin
+
+    public int[][] kClosest(int[][] points, int k) {
+        int m = points.length;
+        int n = points[0].length;
+
+        if(m == 0 && n == 0)
+            return new int[][]{{}};
+        
+        PriorityQueue<int []> pq = new PriorityQueue<>((a,b)->{
+            return ((b[0]*b[0]+b[1]*b[1]) - (a[0]*a[0]+a[1]*a[1])); // other - this, maxPQ
+        });
+
+        for(int i = 0;i < m;i++){
+                pq.add(new int[]{points[i][0],points[i][1]});
+                if(pq.size() > k)
+                pq.remove();
+        }
+
+        int i = 0;
+        int arr[][] = new int[pq.size()][2];
+
+        while(pq.size() > 0){
+            int pair[] = pq.peek();
+            int x = pair[0];
+            int y = pair[1];
+            
+            arr[i][0] = x;
+            arr[i][1] = y;
+
+            i++;
+            pq.remove();
+        }
+        return arr;
+    }
 
 }
